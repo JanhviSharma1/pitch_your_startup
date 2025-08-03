3c5b62ba69deb7f3b333f1870d0bb4fa792bd4fd
Janhvi Sharma <sjanhvi14@gmail.com>
2025-08-04 01:03:21 +0530

    fixed broken code and added search form functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
-        <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+        <h1 className="heading">
           Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>
 
-        <p className="sfont-medium text-[20px] text-white max-w-2xl text-center break-words; !max-w-3xl">
+        <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );
--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -12,10 +12,7 @@ const SearchFormReset = () => {
 
   return (
     <button type="reset" onClick={reset}>
-      <Link
-        href="/"
-        className="size-[50px] rounded-full bg-black flex justify-center items-center !important text-white"
-      >
+      <Link href="/" className="search-btn">
         <X className="size-5" />
       </Link>
     </button>
