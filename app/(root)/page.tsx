1a1aba052f9df4d2d8dbaf8cd6877f2b77f83e0e
Janhvi Sharma <sjanhvi14@gmail.com>
2025-08-03 23:42:26 +0530

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,21 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
-  return <div className="text-30-extrabold ">HOME</div>;
+  return (
+    <>
+      <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sfont-medium text-[20px] text-white max-w-2xl text-center break-words; !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm />
+      </section>
+    </>
+  );
 }
