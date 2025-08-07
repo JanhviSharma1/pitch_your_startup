e3577d8a2ab4bf953215ef58cd90e3d66f103183
Janhvi Sharma <sjanhvi14@gmail.com>
2025-08-08 02:22:23 +0530

    create form

--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,21 @@
-import Ping from "./Ping";
+import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  writeClient
+    .patch(id)
+    .set({ views: totalViews + 1 })
+    .commit()
+    .catch((err) => {
+      console.error("[View] Failed to update view count:", err);
+    });
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
@@ -8,7 +23,7 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">100 views</span>
+        <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
   );
